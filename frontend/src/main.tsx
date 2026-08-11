b1c05d31085780e69f7ea770cb0906fee35b73bf
chmajster <chmajster@gmail.com>
2026-08-12 00:55:17 +0200

    Unify frontend with compact DSM styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/app.css";
 import "./styles/update-transition-fix.css";
+import "./styles/dsm.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
