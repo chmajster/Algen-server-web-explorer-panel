00db0aabd1bc9da08a330f9ff0085456065fccda
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-07 09:03:24 +0200

    refactor shell bridge composition

---
+++ frontend/src/app/shell/ManagedShell.tsx
@@ -0,0 +1,20 @@
+import { SystemContextMenuHost } from "../../components/SystemContextMenuHost";
+import { DesktopEnhancements } from "../DesktopEnhancements";
+import { DesktopWorkspacePortal } from "../DesktopWorkspacePortal";
+import type { DesktopProps } from "../desktop/types";
+import { DesktopContextBridge } from "./DesktopContextBridge";
+import { ShellStateController } from "./ShellStateController";
+import { StartGlobalSearchBridge } from "./StartGlobalSearchBridge";
+import { SystemSearchProviders } from "./SystemSearchProviders";
+
+export function ManagedShell(props: DesktopProps) {
+  return <>
+    <DesktopEnhancements profile={props.profile} t={props.t} toast={props.toast} onSettingsChange={props.onSettingsChange} />
+    <DesktopWorkspacePortal {...props} />
+    <DesktopContextBridge />
+    <SystemSearchProviders profile={props.profile} />
+    <StartGlobalSearchBridge />
+    <ShellStateController />
+    <SystemContextMenuHost />
+  </>;
+}
